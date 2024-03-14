bc96d9805a18853991df0032c28a776d655f04cd
ryanlohyr <ryanloh29@gmail.com>
2024-03-14 19:12:54 +0800

    Add frontend setup

---
+++ frontend/src/app/demo/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from "@/components/ui/button"
+
+const page = () => {
+    return (
+        <div>
+            <Button>Test</Button>
+          Test  
+        </div>
+    );
+};
+
+export default page;
