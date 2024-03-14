c36799d27fab35067d6ca91614ebf0367b67eae4
ryanlohyr <ryanloh29@gmail.com>
2024-03-14 23:55:10 +0800

    Integrate frontend with backend

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, Form
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:3000"],  
+    )
 
 @app.get("/")
 async def root():
--- frontend/src/app/demo/page.tsx
+++ frontend/src/app/demo/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
+import axios from "axios";
 
 enum ArithmeticType {
   Add = "add",
@@ -13,20 +14,50 @@ enum ArithmeticType {
 const Page = () => {
   const [numOne, setNumOne] = useState<number>(0);
   const [numTwo, setNumTwo] = useState<number>(0);
+  const [errror, setError] = useState<string | null>(""); 
   const [result, setResult] = useState<number | null>(null); // null is used to show that the result is not calculated yet
 
-  const calculateResult = (type: string) => {
-    if (type === ArithmeticType.Add) {
-      setResult(numOne + numTwo);
-    } else if (type === ArithmeticType.Subtract) {
-      setResult(numOne - numTwo);
-    } else {
-      // Throw error here
-      console.log("Undefined Type When calculating error");
+  const sendCalculationRequest = async (
+    type: string,
+    num1: number,
+    num2: number
+  ) => {
+    const formData = new FormData();
+    formData.append("num1", String(num1));
+    formData.append("num2", String(num2));
+
+    try {
+      const response = await axios.post(
+        `http://127.0.0.1:8000/${type}`,
+        formData,
+        {
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        }
+      );
+
+      if (isNaN((response.data.result))) {
+        throw Error("result is not a number");
+      } 
+
+      return {
+        data: Number(response.data.result),
+        error: null,
+      };
+    } catch (error) {
+      return { data: null, error: "There was an error processing your request: " + error};
+    }
+  };
+
+  const calculateResult = async (type: string) => {
+    const result = await sendCalculationRequest(type, numOne, numTwo);
+    if (result.error) {
+      setError(result.error);
+      setResult(null);
       return;
     }
-    // Perform post request here
-    
+
+    setError(null);
+    setResult(result.data);
   };
 
   return (
@@ -35,7 +66,8 @@ const Page = () => {
       <div className="flex items-center m-4">
         <Input
           onChange={(event) => {
-            setNumOne(Number(event.target.value));
+            const number = event.target.value ? Number(event.target.value) : 0;
+            setNumOne(number);
           }}
           type="number"
           aria-label="First number"
@@ -43,7 +75,8 @@ const Page = () => {
         -{" "}
         <Input
           onChange={(event) => {
-            setNumTwo(Number(event.target.value));
+            const number = event.target.value ? Number(event.target.value) : 0;
+            setNumTwo(number);
           }}
           type="number"
           aria-label="Second number"
@@ -65,7 +98,8 @@ const Page = () => {
           Substract
         </Button>
       </div>
-      Result: {result ? result : "Not calculated yet"}
+      Result: {result != null ? result : "Not calculated yet"}
+      {errror && <p className="text-red-500">Error occured: <br></br> {errror}</p>}
     </div>
   );
 };
